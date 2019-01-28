80c5c8c65c4b9ee1f0d88a766e90e975d5982f4e
guimel <guimel_g@hotmail.com>
2019-01-28 08:11:56 -0600

    Update api.php

--- rest/api.php
+++ rest/api.php
@@ -7,24 +7,31 @@ try
 //if (isset($_GET['strDBName']) && $_GET['strDBName']!="" ) {
   $db=connectDB();
   if ($db) {
-
     $strOpcion=filter_input(INPUT_GET,'strOpcion');
     if (!$strOpcion){
       $strOpcion=filter_input(INPUT_POST,'strOpcion');
     }
+    $strResultado="";
     switch ($strOpcion) {
       case "GetFechaServer":
         $strSql="SELECT strftime('%d/%m/%Y %H:%M',datetime()) Fecha";
+        $result = $db->query($strSql);
+        $strResultado=json_encode($result->fetchAll(PDO::FETCH_OBJ));
+        break;
+      case "GetWsConfig":
+        $strResultado="localhost:8080/wsPhp/rest/api.php,wswcf.azurewebsites.net,REST";
         break;
       default:
-        $strSql='SELECT * FROM IngProducto';        
+        $strSql='SELECT * FROM IngProducto';
+        $result = $db->query($strSql);
+        $strResultado=json_encode($result->fetchAll(PDO::FETCH_OBJ));
         break;
     }
     //SELECT name FROM sqlite_master WHERE type='table'
     /*$rows= count($result);
     echo "Number of rows: $rows";*/
-    $result = $db->query($strSql);
-    echo json_encode($result->fetchAll(PDO::FETCH_OBJ));
+
+    echo $strResultado;
     /*
     $LoginRS__query=sprintf("SELECT username, password FROM member WHERE username=%s AND password=%s",
     GetSQLValueString($_POST["username"], "text"), GetSQLValueString($password, "text"));
